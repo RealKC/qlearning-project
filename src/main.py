91d6fdda86ffbdcb23db55550831865721d65b30
Mițca Dumitru <dumitru0mitca@gmail.com>
2025-01-06 01:50:10 +0200

    Delete remaining pygame example comments

--- src/main.py
+++ src/main.py
@@ -30,22 +30,16 @@ clock = pygame.time.Clock()
 running = True
 
 while running:
-    # poll for events
-    # pygame.QUIT event means the user clicked X to close your window
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
-    # fill the screen with a color to wipe away anything from last frame
     screen.fill("white")
 
     tilemap.draw(screen)
 
-    # RENDER YOUR GAME HERE
-
-    # flip() the display to put your work on screen
     pygame.display.flip()
 
-    clock.tick(60)  # limits FPS to 60
+    clock.tick(60)
 
 pygame.quit()
